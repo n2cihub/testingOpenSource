b52c4570b9f0dcb30712cb67b694ff778af483aa
Christian Bachl <bachl.christian@gmx.at>
2024-05-13 22:32:20 +0200

    first commit

---
+++ src/main/java/com/example/testingOpenSource/TestingOpenSourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingOpenSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingOpenSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingOpenSourceApplication.class, args);
+	}
+
+}
