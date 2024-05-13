7ea7320ebe1e4eb3d6f5bc66db95c1f66297e855
Christian Bachl <bachl.christian@gmx.at>
2024-05-13 22:41:10 +0200

    Update TestingOpenSourceApplicationTests.java

--- src/test/java/com/example/testingOpenSource/TestingOpenSourceApplicationTests.java
+++ src/test/java/com/example/testingOpenSource/TestingOpenSourceApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.testingOpenSource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TestingOpenSourceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
