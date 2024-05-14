f74ebdfa2a3a02c72f7ca7c9079b05663e456d85
Christian Bachl <bachl.christian@gmx.at>
2024-05-14 15:20:17 +0200

    Update TestingOpenSourceApplication.java

--- src/main/java/com/example/testingOpenSource/TestingOpenSourceApplication.java
+++ src/main/java/com/example/testingOpenSource/TestingOpenSourceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingOpenSourceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TEST");
 		SpringApplication.run(TestingOpenSourceApplication.class, args);
 	}
 
